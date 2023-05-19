d1ccd2027cf3744363001df0033aa8da02f47853
LUKASZ <wieceklukasz12@gmail.com>
2023-05-19 21:47:03 +0200

    4

--- APIGET.py
+++ APIGET.py
@@ -1,12 +1,21 @@
 import requests
 import json
 
-url = 'https://httpbin.org/post'
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+url = 'https://httpbin.org/delay/2'
 headers = {'Content-Type': 'application/json'}
 data = {'name': 'natalia'}
 
+session = requests.Session()
+retry_strategy = Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
+adapter = HTTPAdapter(max_retries=retry_strategy)
+session.mount('https://', adapter)
+
 try:
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = session.post(url, headers=headers, data=json.dumps(data), timeout=1)
 
     response.raise_for_status()
 
